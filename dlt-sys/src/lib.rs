ba078e744e8efad4f66a08bc204cdc65952acbd1
Lilian A. Moraru <lilian.moraru90@gmail.com>
2017-02-27 01:19:53 +0200

    Remove unnecessary dlt_protocol

--- dlt-sys/src/lib.rs
+++ dlt-sys/src/lib.rs
@@ -1,9 +1,8 @@
-#![allow(non_camel_case_types, non_snake_case, dead_code)]
+#![allow(non_camel_case_types, non_snake_case)]
 
 extern crate libc;
 
 pub mod dlt_types;
-pub mod dlt_protocol;
 pub mod dlt_common;
 pub mod dlt_shm;
 pub mod dlt_user;
