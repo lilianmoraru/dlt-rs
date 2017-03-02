61b53438f3d81253186c58ee75488c69d6f8c948
Lilian A. Moraru <lilian.moraru90@gmail.com>
2017-03-02 23:10:12 +0200

    Make build.rs build dlt-daemon using the submodule and link it correctly
    Currently it is not customizable, that will come later

--- dlt-sys/build.rs
+++ dlt-sys/build.rs
@@ -1,13 +1,26 @@
 extern crate gcc;
 extern crate cmake;
 
-use std::env;
-use std::path::{ Path, PathBuf };
-
 fn main() {
-    // Just some hacks currently
-    let build_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
-//    println!("cargo:rustc-link-lib=static=dlt"); // issues with fPIC
-    println!("cargo:rustc-link-lib=dlt");
-    println!("cargo:rustc-link-search=native={}/../../../native", build_dir.display());
+    let dst = cmake::Config::new("dlt-daemon")
+                            .cxxflag("-fPIC")
+                            .define("BUILD_SHARED_LIBS",    "OFF")
+                            .define("WITH_MAN",             "OFF")
+                            .define("WTIH_DLT_ADAPTOR",     "OFF")
+                            .define("WITH_DLT_CONSOLE",     "OFF")
+                            .define("WITH_DLT_SYSTEM",      "OFF")
+                            .define("WITH_DLT_DBUS",        "OFF")
+                            .define("WITH_DLT_EXAMPLES",    "OFF")
+                            .define("WITH_DLT_TESTS",       "OFF")
+                            .define("WITH_DLT_KPI",         "OFF")
+                            .define("DLT_USER",             "Rustaceans")
+                            .define("CMAKE_BUILD_TYPE",     "Release")
+                            .define("CMAKE_INSTALL_PREFIX", "dlt-build")
+                            .build();
+
+    println!("cargo:rustc-link-search=native={}/build/dlt-build/lib/static", dst.display());
+    println!("cargo:rustc-link-lib=static=dlt");
+
+    std::fs::copy(format!("{}/build/dlt-build/bin/dlt-daemon", dst.display()),
+                  format!("{}/../../../dlt-daemon", dst.display())).unwrap();
 }
